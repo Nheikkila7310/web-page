dacfb3b27bcabb6f2d479cf4cdd3654a4b3f55d5
Nicklas Heikkila <nheikkila7@gmail.com>
2022-05-08 10:29:45 -0400

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,67 @@
+import { Facebook, Instagram, Twitter, YouTube } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Phoenix.</Logo>
+        <Desc>Being a Pheonix is the best way to learn</Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <YouTube />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center></Center>
+      <Right></Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://thumbs.dreamstime.com/z/fashion-summer-clothes-clothing-clipart-flat-dress-swimsuit-textile-vacation-outfit-sundress-shorts-trendy-apparel-exact-215845530.jpg",
+    title: "SUMMER SALE",
+    desc: "GET SUMMER READY WITH US TODAY!",
+    bg: "61e04a",
+  },
+  {
+    id: 2,
+    img: "https://unicphscat.blob.core.windows.net/images-prd/s0000757.png",
+    title: "WINTER SALE",
+    desc: "STOCK UP FOR THE COLD WEATHER",
+    bg: "61d2e8",
+  },
+  {
+    id: 3,
+    img: "https://images.squarespace-cdn.com/content/v1/5d4c6077a7991a0001837faa/1598886601477-6R4HCVVQSS1MJ476HE23/casual+family+fall+2+KMP+EDIT-01.png?format=1000wg",
+    title: "FALL SALE",
+    desc: "GET READY FOR THE LEAVES",
+    bg: "b5895b",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg",
+    title: "WONDERFUL WINTERWEAR!",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+  {
+    id: 2,
+    img: "https://www.pngarts.com/files/3/Clothing-Download-PNG-Image.png",
+  },
+  {
+    id: 3,
+    img: "https://www.pngarts.com/files/3/Clothing-Download-Transparent-PNG-Image.png",
+  },
+  {
+    id: 4,
+    img: "https://www.pngarts.com/files/3/Clothing-Transparent.png",
+  },
+  {
+    id: 5,
+    img: "https://www.pngarts.com/files/3/Clothing-Free-PNG-Image.png",
+  },
+  {
+    id: 6,
+    img: "https://www.pngarts.com/files/3/Clothing-PNG-Picture.png",
+  },
+  {
+    id: 7,
+    img: "https://www.pngarts.com/files/3/Clothing-PNG-Free-Download.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Clothing-Transparent-Images.png",
+  },
+];
